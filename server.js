5aef7d2ea6b79037267549f455377ca53cc85aa7
Tyler Nichols <tylernichols@Tylers-MBP-2.htdc.org>
2017-01-21 08:36:31 -1000

    added event listener

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection',(socket) => {
-  console.log('a user connected');
-  socket.on('disconnect',() => {
-    console.log('user disconnected');
+  socket.on('chat message',(msg) => {
+    console.log('message: ' + msg);
   });
 });
 
