db325b2d047fcf8a901c6226048d48cedbec0fdb
Andrea Takamiya <Andrea@Andreas-Air.htdc.org>
2017-01-21 11:00:30 -1000

    Added get code enter code

--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const home = require('../routes/home');
 const router = express.Router();
+
 
 router.route('/')
   .get((req, res) => {
     res.render('templates/home');
-  })
+  });
 
+router.route('/get-code')
+  .get((req, res) => {
+    let code = 123;
+    console.log(code);
+
+    res.render('templates/get-code', {"code": code});
+  });
 
 router.route('/play')
   .get((req, res) => {
     res.render('templates/play');
-  })
+  });
+
+
 
 module.exports = router;
