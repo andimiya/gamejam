273c0abf56f1533c12fe490681b9cb26261e3ee9
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-01-21 08:42:17 -1000

    added event listener

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',(socket) => {
-  socket.on('chat message',(msg) => {
-    console.log('message: ' + msg);
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
