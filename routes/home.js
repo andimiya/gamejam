a9d97a322e7ff047e4f93ffa3eb1077ccfd280e3
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-01-21 10:39:48 -1000

    added routes

---
+++ routes/home.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/')
+  .get((req, res) => {
+    res.render('templates/home');
+  })
+
+
+router.route('/play')
+  .get((req, res) => {
+    res.render('templates/play');
+  })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,18 +2,36 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const home = require('./routes/home');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+app.set('view engine', '.hbs');
+
+app.engine('.hbs', exphbs({
+  extname:'.hbs',
+  defaultLayout:'main',
+}))
+
+app.use('/', home);
 
 
-http.listen(3000, function(){
+
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) =>{
+//     io.emit('chat message', msg);
+//   });
+// });
+
+
+app.listen(3000, function(){
   console.log('listening on *:3000');
 });
