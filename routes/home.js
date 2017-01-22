cda892dc86182419472dc01094abed80a6353c82
Andrea Takamiya <Andrea@Andreas-Air.htdc.org>
2017-01-21 14:15:25 -1000

    Added a lobby

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const home = require('../routes/home');
 const router = express.Router();
+const io = require('socket.io');
+
 
 let generatedCode = Math.floor(Math.random()*100000);
 
 const isCodeValid = (req, res, next) => {
-  if(parseInt(req.body.code) !== generatedCode) {
-    res.redirect('/')
+  if(parseInt(req.body.code) === generatedCode) {
+
+    res.redirect('/');
   } else {
     next();
   }
-}
+};
 
 router.route('/')
   .get((req, res) => {
@@ -22,13 +25,22 @@ router.route('/get-code')
     res.render('templates/get-code', {"code": generatedCode});
 });
 
-router.route('/play')
+router.route('/lobby')
+  .get((req, res) => {
+  res.render('templates/lobby');
+});
+
+router.route('/lobby')
   .post(isCodeValid, (req, res) => {
-    res.redirect('play');
-})
- .get((req, res) => {
-    res.render('templates/play');
-  });
+  res.redirect('lobby');
+});
+
+router.route('/play')
+  .get((req, res) => {
+  res.render('templates/play');
+});
+
+
 
 
 
